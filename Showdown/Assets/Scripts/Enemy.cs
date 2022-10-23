b404989c64630cf00bb08fe6950a5ffc758a266f
akshi <neni.akki@gmail.com>
2022-10-23 01:48:11 -0400

    Fixed enemy death animation

--- Showdown/Assets/Scripts/Enemy.cs
+++ Showdown/Assets/Scripts/Enemy.cs
@@ -7,6 +7,10 @@ public class Enemy : MonoBehaviour
     public float speed;
     public Transform target; 
     public float minimumDistance; 
+    public int maxHealth = 100; 
+    int currentHealth; 
+
+    public Animator anim; 
     
     // Start is called before the first frame update
     void Start()
@@ -22,5 +26,24 @@ public class Enemy : MonoBehaviour
         } else {
             //ATTACK CODE
         }
+    }
+
+    public void TakeDamage(int damage){
+        currentHealth -= damage; 
+        anim.SetTrigger("hurt");
+        transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime); 
+        //Play hurt animation
+        if(currentHealth <= 0){
+            Die();
+        }
+    }
+
+    void Die() {
+        Debug.Log("Enemy died!"); 
+        //Die animation
+        anim.SetBool("isDead", true); 
+        //Disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;  
     }
 }
--- Showdown/Assets/Scripts/PlayerCombat.cs
+++ Showdown/Assets/Scripts/PlayerCombat.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class PlayerCombat : MonoBehaviour
 {
     public Animator anim; 
+    public Transform attackPoint; 
+    public float attackRange = 0.5f; 
+    public LayerMask enemyLayers; 
+    public int attackDamage = 10; 
+
+    public float attackRate = 2f; 
+    float nextAttackTime = 0f; 
 
     // Start is called before the first frame update
     void Start()
@@ -15,13 +22,30 @@ public class PlayerCombat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-            Attack(); 
+        if(Time.time >= nextAttackTime){
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Attack(); 
+                nextAttackTime = Time.time + 1f / attackRate; 
+            }
         }
     }
 
     void Attack(){
         //Play an attack animation
         anim.SetTrigger("attack"); 
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        foreach(Collider2D enemy in hitEnemies){
+            Debug.Log("We hit" + enemy.name); 
+            enemy.GetComponent<Enemy>().TakeDamage(attackDamage); 
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if(attackPoint == null){
+            return; 
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange); 
     }
 }
